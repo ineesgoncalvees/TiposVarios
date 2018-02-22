afa61374b67b4caf6eccad40302f183c99ee3b48
Ines <ines98goncalves@gmail.com>
2018-02-22 10:10:27 +0000

    Atualizar

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TiposVarios
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace TiposVarios {
+    class Program {
+        static void Main(string[] args) {
+            string a = "kjhgfd";
+            string b = "jhgfd";
+            string c = "\u2605";
+            string d = @"""werthyjhtgref""";
+            string e = @"\\u2344";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
         }
     }
 }
